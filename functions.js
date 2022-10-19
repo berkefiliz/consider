6c579b74eed33ea512db8c93f08b65bef1258301
Berke Filiz <edu.berkefiliz@gmail.com>
2022-10-19 20:21:18 +0200

    Force reload CSS

--- functions.js
+++ functions.js
@@ -45,3 +45,16 @@ function makeArticlesList() {
         `;
     }
 }
+
+function forceRefreshCSS() {
+    var h, a, f;
+    a = document.getElementsByTagName("link");
+    for (h = 0; h < a.length; h++) {
+        f = a[h];
+        if (f.rel.toLowerCase().match(/stylesheet/) && f.href) {
+            var g = f.href.replace(/(&|\?)rnd=\d+/, "");
+            f.href = g + (g.match(/\?/) ? "&" : "?");
+            f.href += "rnd=" + new Date().valueOf();
+        }
+    }
+}
