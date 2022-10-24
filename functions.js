2acc814ab65e1bbbd00341de98b647bd19f3706c
Berke Filiz <edu.berkefiliz@gmail.com>
2022-10-24 15:39:24 +0200

    Partial automatization for post layout

--- functions.js
+++ functions.js
@@ -1,3 +1,36 @@
+POSTS = [
+    {
+        title: "We are all livestock animals for trees",
+        tab: "",
+        date: "2022/--/--",
+        link: null,
+    },
+    {
+        title: "Moon landing was an alien invasion",
+        tab: "",
+        date: "2022/11/--",
+        link: null,
+    },
+    {
+        title: "Showers are a hoax created by the government",
+        tab: "",
+        date: "2022/11/--",
+        link: null,
+    },
+    {
+        title: "Our socks could be interdimensional overlords",
+        tab: "Sock overlords",
+        date: "2022/10/23",
+        link: "0002_sock_overlords.html",
+    },
+    {
+        title: "Vegan tomatoes do not contain animal products",
+        tab: "Vegan tomatoes",
+        date: "2022/10/14",
+        link: "0001_vegan_tomatoes.html",
+    },
+];
+
 function makeArticlesList() {
     const searchbar = document.getElementById("searchbar");
     const articles = document.getElementById("articles");
@@ -44,4 +77,17 @@ function makeArticlesList() {
             </div>
         `;
     }
+}
+
+function preparePost(i) {
+    let post = POSTS[POSTS.length - i];
+    let header = document.getElementById("header");
+    header.innerHTML = `
+        <a href="/index.html" id="header-text">< Get out</a>
+        <h2 id="subheader"></h2>
+    `
+    let subheader = document.getElementById("subheader");
+    let tabtitle = document.getElementById("tabtitle");
+    subheader.innerHTML = post.title;
+    tabtitle.innerHTML = "Consider: " + post.tab;
 }
